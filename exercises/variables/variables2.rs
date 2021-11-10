6f086c6cbd1574c6691d4cdb20f9c4068892eb41
OyinloluB <oyinloluwab@gmail.com>
2021-11-10 22:51:30 +0100

    variable exercise done

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -1,10 +1,19 @@
 // variables2.rs
 // Make me compile! Execute the command `rustlings hint variables2` if you want a hint :)
 
-// I AM NOT DONE
-
 fn main() {
-    let x;
+    // What happens if you annotate line 7 with a type annotation?
+    // let x : u32;
+
+    // What if you give x a value?
+    // let x = 0;
+
+    // What if you do both?
+    let x: u32 = 0;
+
+    // What type should x be, anyway?
+    // An integer because different types cannot be compared
+
     if x == 10 {
         println!("Ten!");
     } else {
