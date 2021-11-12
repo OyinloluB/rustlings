efbfb8040b228ae2438f29f4cdf1dbef02abc666
OyinloluB <oyinloluwab@gmail.com>
2021-11-12 18:15:40 +0100

    structs exercises complete

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -17,6 +17,7 @@ impl Package {
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
             // Something goes here...
+            panic!();
         } else {
             Package {
                 sender_country,
@@ -26,12 +27,12 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        &self.sender_country != &self.recipient_country
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_kg: i32) -> i32 {
+        (&self.weight_in_grams * cents_per_kg + (1000 / 2)) / 1000
     }
 }
 
@@ -73,10 +74,10 @@ mod tests {
         let sender_country = String::from("Spain");
         let recipient_country = String::from("Spain");
 
-        let cents_per_gram = 3;
+        let cents_per_kg = 3000;
 
         let package = Package::new(sender_country, recipient_country, 1500);
 
-        assert_eq!(package.get_fees(cents_per_gram), 4500);
+        assert_eq!(package.get_fees(cents_per_kg), 4500);
     }
 }
